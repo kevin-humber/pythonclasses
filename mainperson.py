3fcb797124ea4126c45211249390ae60b3f05803
kevin-humber <122173416+kevin-humber@users.noreply.github.com>
2023-03-27 13:48:25 -0400

    Add files via upload

---
+++ mainperson.py
@@ -0,0 +1,9 @@
+from person import Person
+
+person1 = Person("Alice", 30)
+person2 = Person("Bob", 25)
+# we have created two person objects
+
+# and now with each object, we can call any methods associated with that objects, in this case introduce()
+person1.introduce()
+person2.introduce()
